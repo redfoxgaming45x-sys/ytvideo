dfee1c7c1fe8db69ba6f90c34c8fdbea370abe8a
redfoxgaming45x-sys <redfoxgaming45x@gmail.com>
2026-04-14 17:17:53 +0600

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const io = socketIo(server, {
       'http://localhost:3000',
       'http://localhost:5500',
       'http://127.0.0.1:5500'
-      'http://localhost:8080'
+  
     ],
     methods: ['GET', 'POST'],
     credentials: true
